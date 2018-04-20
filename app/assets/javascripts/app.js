a74cbfab020f7b5b80ac3bef10d27751f934d4ee
matt6390 <matthewstone639@gmail.com>
2018-04-19 19:37:22 -0500

    216 - Removed the search button, and allowed for real-time search for first_nm, last_nm and email

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,25 +1,39 @@
 /* global Vue */
-document.addEventListener("DOMContentLoaded", function(event) { 
+document.addEventListener("DOMContentLoaded", function(event) {
   var app = new Vue({
-    el: '#app',
+    el: "#app",
     data: {
       leads: [],
+      leadFilter: "",
       time_format: "12/25/17",
       url: "https://www.google.com/"
     },
     mounted: function() {
-      $.get('/api/v1/leads.json').success(function(response) {
-        console.log(this);
-        this.leads = response;
-      }.bind(this));
+      $.get("/api/v1/leads.json").success(
+        function(response) {
+          console.log(this);
+          this.leads = response;
+        }.bind(this)
+      );
     },
     methods: {
       moment: function(date) {
         return moment(date);
+      },
+      isValidLead: function(lead) {
+        console.log(lead);
+        var validLead = null;
+        var filter = this.leadFilter.toLowerCase();
+        if (
+          lead.first_name.toLowerCase().includes(filter) ||
+          lead.last_name.toLowerCase().includes(filter) ||
+          lead.email.toLowerCase().includes(filter)
+        ) {
+          validLead = lead;
+        }
+        return validLead;
       }
     },
-    computed: {
-
-    },
+    computed: {}
   });
 });
