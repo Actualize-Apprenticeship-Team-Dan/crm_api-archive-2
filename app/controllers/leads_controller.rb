aaa5be9cbf0449368b189e86dc2f901c1213cae8
abes1809 <amandanicole321@gmail.com>
2018-05-03 18:55:36 -0500

    got rid of g

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -47,7 +47,7 @@ class LeadsController < ApplicationController
                   :to   => @lead.phone,
                   :from => ENV['TWILIO_PHONE_NUMBER']
     })
-    @messages = (messages_from_lead + messages_from_call_converter)g
+    @messages = (messages_from_lead + messages_from_call_converter)
   end
 
   def update
